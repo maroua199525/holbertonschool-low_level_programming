aa41feacc6e026ac4fc0994b28729a1a3108f25b
maroua199525 <2646@holbertonschool.com>
2021-02-04 17:46:14 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+i = 0;
+while (i < 10)
+{
+printf("%d", i);
+i++;
+}
+printf("%d");
+return (0);
+}

3ad380c91d7da8c3731bfd181f38e30b480d0985
maroua199525 <2646@holbertonschool.com>
2021-02-04 17:08:50 +0000

    Alphabitic

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,15 +8,14 @@
 */
 int main(void)
 {
-char ch1;
-char ch2;
-for (ch1 = 'a' ; ch1 <= 'z' ; ch1++)
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch1++)
 {
-putchar(ch1);
+putchar(ch);
 }
-for (ch2 = 'a' ; ch2 <= 'z' ; ch2++)
+for (ch = 'a' ; ch <= 'z' ; ch2++)
 {
-putchar(ch2);
+putchar(ch);
 }
 putchar('\n');
 return (0);

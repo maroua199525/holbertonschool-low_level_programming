93ec4837e193586d5330b4c3d3727a916011c509
maroua199525 <2646@holbertonschool.com>
2021-02-04 13:17:53 +0000

    if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,13 @@ if (n > 0)
 {
 printf("%d is positive\n", n);
 }
-else if (n == 0)
+else if (n < 0)
 {
-printf("%d is zero\n", n);
+printf("%d is negative\n", n);
 }
 else
 {
-printf("%d is negative\n", n);
+printf("%d is zero\n", n);
 }
 return (0);
 }

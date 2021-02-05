dede583652e8cbc4e4b5c732ab1620a9399de25c
maroua199525 <2646@holbertonschool.com>
2021-02-05 19:58:41 +0000

    combibaison

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i <= 98; i++)
+{
+for (j = 1; j <= 99; j++)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+if ((i == 98) && (j == 99))
+{
+putchar('\n');
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}

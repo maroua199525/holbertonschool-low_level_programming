f68e32a141147b305d6a7e96a9cb3ca1e9a8d010
maroua199525 <2646@holbertonschool.com>
2021-02-05 20:52:33 +0000

    combinaison

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,17 +2,17 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int i;
 int j;
 for (i = 0; i <= 98; i++)
 {
-for (j = 1; j <= 99; j++)
+for (j = i+1; j <= 99; j++)
 {
 putchar((i / 10) + '0');
 putchar((i % 10) + '0');

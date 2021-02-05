05c29f3587ea2abab67fc02eeb218f63852772c3
maroua199525 <2646@holbertonschool.com>
2021-02-05 22:12:45 +0000

    combinaison

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int i;
 int j;
 for (i = 0; i <= 98; i++)
 {
-for (j = i+1; j <= 99; j++)
+for (j = i + 1; j <= 99; j++)
 {
 putchar((i / 10) + '0');
 putchar((i % 10) + '0');
@@ -20,14 +20,9 @@ putchar(' ');
 putchar((j / 10) + '0');
 putchar((j % 10) + '0');
 if ((i == 98) && (j == 99))
-{
-putchar('\n');
-}
-else
-{
+continue;
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar('\n');

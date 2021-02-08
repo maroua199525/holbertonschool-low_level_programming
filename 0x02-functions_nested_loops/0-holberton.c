715627d5553b21a05d3c6c697ca159bfbc3c9e72
maroua199525 <2646@holbertonschool.com>
2021-02-08 09:52:30 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+putchar('H');
+putchar('o');
+putchar('l');
+putchar('b');
+putchar('e');
+putchar('r');
+putchar('t');
+putchar('o');
+putchar('n');
+putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+Helo, today we will learn nested loops and how to use them.
+What is a function and how do you use functions?
+What is the difference between a declaration and a definition of a function?
+What is a prototype?

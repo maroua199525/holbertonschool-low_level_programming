bd4efe35d31c6991eb3a5cfea168ae0256b6ab56
maroua199525 <2646@holbertonschool.com>
2021-02-09 08:23:18 +0000

    prtototype

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,13 @@
 */
 void print_alphabet(void)
 {
-char ch;
-for (ch = '97'; ch <= '122'; ch++)
+int i;
+for (i = 97; i <= 122; i++)
 {
-_putchar(ch);
+_putchar(i);
 _putchar('\n');
 }
 return;
 }
+
 
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,24 +6,20 @@
 */
 void jack_bauer(void)
 {
-int i;
-for (i = 48; i <= 57; i++)
+int i, j;
+for (i = 0; i <= 23; i++)
 {
-_putchar(48);
-_putchar(48);
-_putchar(58);
-_putchar(48);
-_putchar(i);
-_putchar('\n');
-}
-for (i = 48; i <= 57; i++)
+for (j = 0; j <= 59; j++)
 {
-_putchar(50);
-_putchar(51);
-_putchar(58);
-_putchar(53);
-_putchar(i);
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((i % 10) + '0');
 _putchar('\n');
 }
 }
+}
+
+
 

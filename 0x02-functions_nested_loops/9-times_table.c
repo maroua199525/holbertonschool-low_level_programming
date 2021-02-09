d6dbfa168f1c7596342f5ee178c480825b1d5cb0
maroua199525 <2646@holbertonschool.com>
2021-02-09 16:14:20 +0000

    prototype

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,35 +6,35 @@
 */
 void times_table(void)
 {
-int i, j, r;
-		for (i = 0; i <= 9; i++)
-		{
-			for (j = 0; j <= 9; j++)
-			{
-					r = (i * j);
-					if (j == 0)
-						{
-						_putchar('0');
-							}
-					else if (r < 10)
-						{
-						_putchar(' ');
-						_putchar(r + '0');
-						}
-					else
-						{
-						_putchar((r / 10) + '0');
-						_putchar((r % 10) + '0');
-							}
-					if (j < 9)
-						{
-						_putchar(',');
-						_putchar(' ');
-						}
-							else
-							{
-							putchar ('\n');
-							}
-						}
-		}
+int i, j;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+r = (i * j);
+if (j == 0)
+{
+_putchar('0');
+}
+else if (r < 10)
+{
+_putchar(' ');
+_putchar(r + '0');
+}
+else
+{
+_putchar((r / 10) + '0');
+_putchar((r % 10) + '0');
+}
+if (j < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+putchar ('\n');
+}
+}
+}
 }

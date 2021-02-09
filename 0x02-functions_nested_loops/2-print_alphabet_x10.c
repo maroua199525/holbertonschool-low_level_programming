d52f26f616b0bf7eafc411fe577c401be68ca325
maroua199525 <2646@holbertonschool.com>
2021-02-09 08:45:05 +0000

    prototype

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@
 void print_alphabet_x10(void)
 {
 int i, j;
-for (i = 0; i <= 10; i++)
+for (i = 0; i < 10; i++)
 {
 for (j = 97; j <= 122; j++)
 {
@@ -16,6 +16,10 @@ _putchar(j);
 _putchar('\n');
 }
 }
+
+
+
+
 
 
 

a48df14b74647afe4397ded4643c7d87770fd496
maroua199525 <2646@holbertonschool.com>
2021-02-10 17:37:15 +0000

    table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,30 +17,30 @@ void print_times_table(int n)
 			{
 				r = (i * j);
 				if (j == 0)
-				{
 					_putchar('0');
-				}
 				else if (r < 10)
 				{
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar(r + '0');
 				}
 				else if (r < 100)
 				{
+					_putchar(' ');
+					_putchar(' ');
 					_putchar((r / 10) + '0');
 					_putchar((r % 10) + '0');
 				}
 				else
 				{
+					_putchar(' ');
 					_putchar((r / 100) + '0');
 					_putchar(((r % 100) / 10) + '0');
 					_putchar(((r % 100) % 10) + '0');
 				}
 				if (j < n)
-				{
 					_putchar(',');
-					_putchar(' ');
-				}
 				else
 					_putchar ('\n');
 			}

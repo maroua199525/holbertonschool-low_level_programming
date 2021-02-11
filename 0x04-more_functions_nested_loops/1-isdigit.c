939941567931bbe271be74fce55669f0e34ed259
maroua199525 <2646@holbertonschool.com>
2021-02-11 11:08:04 +0000

    more loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* _isdigit - check an digit
+*@c:integer
+* Return: either 0 or 1.
+*/
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* print_numbers - print numbers
+*
+* Return;
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+		_putchar('\n');
+}

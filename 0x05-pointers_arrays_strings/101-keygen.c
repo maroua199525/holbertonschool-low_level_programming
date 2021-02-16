89a21a88f3ee768a5bc3c5d2669768014ec6ee7e
maroua199525 <2646@holbertonschool.com>
2021-02-16 18:57:53 +0000

    password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+/**
+ *main - print password
+ *Return: n
+ */
+int main(void)
+{
+	srand((unsigned int)(time(NULL)));
+	int i;
+	char pass[12];
+
+	printf("Press enter to get a twelve-character password\n");
+	getchar();
+
+		for (i = 0; i < 4; i++)
+		{
+			pass[i] = rand() % 9;
+			char capLetter = 'A' + (rand() % 26);
+
+			pass[i + 2] = capLetter;
+			char letter = 'a' + (rand() % 26);
+
+			pass[i + 3] = letter;
+			printf("%d%c%c", pass[i], pass[i + 2], pass[i + 3]);
+		}
+	printf("\n\n");
+}

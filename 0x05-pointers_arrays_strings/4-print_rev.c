6231e3f03d178aab52210490ba11439abcf34f13
maroua199525 <2646@holbertonschool.com>
2021-02-16 09:23:10 +0000

    string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_rev - rev the string
+ *@s:a string
+ * Return
+ */
+void print_rev(char *s)
+{
+	int i, j, length;
+
+	length = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		length = length + 1;
+		i++;
+	}
+	j = length;
+	while (j >= 0)
+	{
+		_putchar(s[j]);
+		j--;
+	}
+	_putchar('\n');
+}

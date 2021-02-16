486c15a491408f021e2102a0e619f4c896d7506c
maroua199525 <2646@holbertonschool.com>
2021-02-16 13:34:47 +0000

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,20 +11,19 @@ void rev_string(char *s)
 
 	length = 0;
 	i = 0;
-	while (s[i + 1] != '\0')
+	while (*(s + i) != '\0')
 	{
 		length = length + 1;
 		i++;
 	}
 	j = length - 1;
 	i = 0;
-	if (length != 0)
 	{
 		while (i <= length / 2)
 		{
-			ch = s[i];
-			s[i] = s[j - i];
-			s[j - i] = ch;
+			ch = *(s + i);
+			*(s + i) = *(s + j - i);
+			*(s + j - i) = ch;
 			i++;
 		}
 	}

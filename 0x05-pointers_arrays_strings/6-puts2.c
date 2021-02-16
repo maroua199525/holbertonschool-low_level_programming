8fe8f629570b12bafc60f0a64ecf01e2933d334a
maroua199525 <2646@holbertonschool.com>
2021-02-16 09:29:23 +0000

    string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * puts2 - puts string
+ *@str:string
+ * Return
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+	_putchar('\n');
+}

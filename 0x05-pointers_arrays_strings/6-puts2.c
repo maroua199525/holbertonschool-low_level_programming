a2b3f1184c62a1944446a2ce5e531003ca3ca6e7
maroua199525 <2646@holbertonschool.com>
2021-02-16 10:37:35 +0000

    string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,8 +12,12 @@ void puts2(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i = i + 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
 	}
 	_putchar('\n');
 }
+		

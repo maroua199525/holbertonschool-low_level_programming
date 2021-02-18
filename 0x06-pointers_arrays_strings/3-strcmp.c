9d923aff2c90242c9d99789402e1135d405573db
maroua199525 <2646@holbertonschool.com>
2021-02-18 17:30:25 +0000

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,15 +10,16 @@ int _strcmp(char *s1, char *s2)
 	int i, p;
 
 	i = 0;
-	p = 0;
-	if (s1[i] != '\0')
+	while (s1[i] != '\0' && s1[i] != '\0')
 	{
-		if (s2[i] == '\0')
-			p = s1[i];
-		else if (s1[i] == s2[i])
+		if (s1[i] == s2[i])
 			p = 0;
 		else
+		{
 			p =  (s1[i] - s2[i]);
+			s1[i + 1] = '\0';
+		}
+		i++;
 	}
 	return (p);
 }

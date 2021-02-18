520979196fc50de70fe502908dce7ebf16e7f319
maroua199525 <2646@holbertonschool.com>
2021-02-18 13:48:45 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * *_strcpy - copies the string
+ *@src:string
+ *@dest:string
+ *@n:integer
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+ 
+		i = 0;
+		for (i = 0; i <= n; i++)
+		{
+			dest[i] = src[i];
+		}
+	return (dest);
+}

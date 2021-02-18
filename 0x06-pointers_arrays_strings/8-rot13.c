84d6706e4689163f929034d6c1295f0070cdd964
maroua199525 <2646@holbertonschool.com>
2021-02-18 21:12:22 +0000

    rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,25 +1,25 @@
 #include "holberton.h"
 /**
- * *string_toupper - uppercase the string
+ * *rot13 - rot string
  *@str:string
- *Return:str;
+ *Return:str
  */
-char *string_toupper(char *str)
+char *rot13(char *str)
 {
 	int i, j;
-	char ch[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-				'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-				'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-				 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-				 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char ch1[] = "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz";
+	char ch2[] = "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm";
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		for (j = 0; j <= 52; j++)
+		for (j = 0; j < 52; j++)
 		{
-			if (str[i] == ch[j])
-				str[i] = ch[j] + 13;
+			if (str[i] == ch1[j])
+			{
+				str[i] = ch2[j];
+				break;
+			}
 		}
 		i++;
 	}

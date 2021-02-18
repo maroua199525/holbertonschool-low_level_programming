07872ddda0dea6eccee84b08b19e6af8e6ea2d5b
maroua199525 <2646@holbertonschool.com>
2021-02-18 13:01:58 +0000

    stract

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+/**
+ * _strncat - concat two strings
+ *@src:a string
+ *@dest:a string
+ *@n: integer
+ * Return:dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		i++;
+	}
+	for (j = 0; j < n; j++)
+	{
+		dest[i + j + 1] = src[j];
+	}
+	dest[i + j + 1] = '\0';
+	return (dest;
+}

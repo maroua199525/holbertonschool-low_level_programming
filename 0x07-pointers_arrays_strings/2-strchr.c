f1fa88d990b57410ec687c211c08e7ce3c964a89
maroua199525 <2646@holbertonschool.com>
2021-02-22 16:26:54 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * *_strchr - locates a character in a string
+ *@s:string
+ *@c: caracter
+ *Return:s;
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != c)
+	{
+		i++;
+	}
+	i = i;
+	while (s[i] != '\0')
+	{
+		return (&s[i]);
+		i++;
+	}
+	
+	return (0);
+}

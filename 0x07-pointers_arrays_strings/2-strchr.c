33011f17e5c820757c86a7ff0c735edf0af88dc4
maroua199525 <2646@holbertonschool.com>
2021-02-22 17:03:49 +0000

    pointrs

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,12 @@ char *_strchr(char *s, char c)
 	int i;
 
 	i = 0;
-	while (s[i] != c)
-	{
-		i++;
-	}
-	i = i;
 	while (s[i] != '\0')
 	{
-		return (&s[i]);
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
 		i++;
 	}
 	

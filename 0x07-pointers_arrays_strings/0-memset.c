53ce5e000d13697184aa1dfee4d69840f4162562
maroua199525 <2646@holbertonschool.com>
2021-02-22 16:25:14 +0000

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * *_memset - fills memory with a constant byte
+ *@s:string
+ *@b:string
+ *@n:unsigned int
+ *Return:s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	
+	i = 0;
+	while (i < n)
+	{
+		if (i < n)
+			*(s + i) = b;
+		i++;
+	}
+	return (s);
+}

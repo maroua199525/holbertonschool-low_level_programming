db325e065b152c67bd6d4e022af4834e87141564
maroua199525 <2646@holbertonschool.com>
2021-02-22 19:03:45 +0000

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,21 +7,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
-
+	int i, j, k;
+	
+	k = 0;
 	i = 0;
-	while (s[i] != '\0')
+	while (s[i] != '\0' && s[i] != ' ')
 	{
 		j = 0;
 		while (accept[j] != '\0')
 		{
 			if (s[i] == accept[j])
 			{
-				j = j + 1;
+				k = k + 1;
 			}
 			j++;
 		}
 		i++;
 	}
-	return (j + 1);
+	return (k);
 }

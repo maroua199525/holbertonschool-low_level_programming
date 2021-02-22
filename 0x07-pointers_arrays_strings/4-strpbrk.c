690ed5a81c7ff61348a0e3aa5ff69f9dd330e661
maroua199525 <2646@holbertonschool.com>
2021-02-22 18:12:58 +0000

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * *_strpbrk - locates a string in a string
+ *@s:string
+ *@accept:string
+ *Return:s;
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] >= '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}

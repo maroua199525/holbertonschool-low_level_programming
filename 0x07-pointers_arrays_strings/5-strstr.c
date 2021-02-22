1f551be8f75ba4f0ad1a7c384a78aa771ac3e9e8
maroua199525 <2646@holbertonschool.com>
2021-02-22 18:43:52 +0000

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * *_strstr - locates a string in a string
+ *@haystack:string
+ *@needle:string
+ *Return:s;
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	i = 0;
+	while (haystack[i] != '\0')
+		{
+			j = 0;
+			while (needle[j] != '\0')
+				{
+					if (haystack[i] == needle[j])
+						{
+							return (&haystack[i]);
+						}
+					i++;
+				}
+			j++;
+		}
+	return (0);
+}

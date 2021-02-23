4e09784f7f719aaa1b94c5b1c80841c98a38f41c
maroua199525 <2646@holbertonschool.com>
2021-02-23 13:58:45 +0000

    print

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,5 +17,5 @@ void print_diagsums(int *a, int size)
 		sum1 = sum1 + *(a + size * i + i);
 		sum2 = sum2 + *(a + i * size + size - i - 1);
 	}
-	printf("%d %d\n", sum1, sum2);
+	printf("%i %i\n", sum1, sum2);
 }

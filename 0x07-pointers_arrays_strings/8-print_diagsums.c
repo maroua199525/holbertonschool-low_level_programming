79a7ff14fe91bf8f3c6bac5381793f5cbedd2f1a
maroua199525 <2646@holbertonschool.com>
2021-02-23 13:52:28 +0000

    print

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * print_diagsums - print diagsums
  *@a:integer
@@ -13,7 +14,8 @@ void print_diagsums(int *a, int size)
 	sum2 = 0;
 	for (i = 0; i < size; i++)
 	{
-		sum1 = sum1 + a[i][size - 1 - i];
-		sum2 = sum2 + a[size - 1 - i][size - 1 - i];
+		sum1 = sum1 + *(a + size * i + i);
+		sum2 = sum2 + *(a + i * size + size - i - 1);
 	}
+	printf("%d %d\n", sum1, sum2);
 }

690b1b71207a57eaeb31fd86197324162c200e42
maroua199525 <2646@holbertonschool.com>
2021-02-24 20:54:43 +0000

    is

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * factorial - print factorial
+ *@n:integer
+ * Return:m
+ */
+int factorial(int n)
+{
+	int m;
+
+	if ((n - 1) < 0)
+		return (0);
+	if ((n - 1) == 0)
+		m = 0;
+	else
+		m = ((n - 1) * factorial(n - 2));
+	if ((m + 1) % n == 0)
+		return (1);
+	else
+		return (0);
+}
+#include "holberton.h"
+/**
+ * factorial - print factorial
+ *@n:integer
+ * Return:m
+ */
+int is_prime_number(int n)
+{
+	return (factorial(n));
+}

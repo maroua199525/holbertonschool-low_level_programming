1a5d7a59213a77914e264d024704d79e56725d67
maroua199525 <2646@holbertonschool.com>
2021-02-24 12:41:39 +0000

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 /**
  *	_puts_recursion - print string
- *@s:integer
+ *@s:string
  * Return
  */
 void _puts_recursion(char *s)
 {
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
+	if (*s == '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	}
 }

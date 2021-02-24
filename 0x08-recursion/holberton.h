1e113c3f292bf987c3d267b915bd3515971c82a3
maroua199525 <2646@holbertonschool.com>
2021-02-24 21:45:12 +0000

    recursion

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -7,7 +7,10 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int _is_sqrt(int i, int b);
+int is_palind_drome(char *s, int m, int j);
 int is_prime_number(int n);
+int factorial(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 #endif

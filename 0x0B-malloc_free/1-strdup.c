8d296767b78ab557b6a74949dea1c590caffa56e
maroua199525 <2646@holbertonschool.com>
2021-03-02 14:58:19 +0000

    fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,21 +9,17 @@ char *_strdup(char *str)
 {
 	int i, len;
 	char *p;
-
+	if (str == NULL)
+		return (NULL);
 	len = 0;
 	while (str[len] != '\0')
 		len++;
 
-	if (str == NULL)
-		return (NULL);
 	p = malloc(sizeof(char) * len + 1);
 	if (p == NULL)
 		return (NULL);
-	else
-	{
-		for (i = 0;str[i]; i++)
+	for (i = 0;str[i]; i++)
 			p[i] = str[i];
-	}
 	p[i] = '\0';
 	return (p);
 }

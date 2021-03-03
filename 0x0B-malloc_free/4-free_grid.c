72b59d981e0d691304b72bfda4b027b3533079bd
maroua199525 <2646@holbertonschool.com>
2021-03-03 17:18:57 +0000

    free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,14 +4,13 @@
  * free_grid - free an array
  *@grid:integer
  *@height:integer
- *Return: 
+ *Return:
  */
 void free_grid(int **grid, int height)
 {
 	int i;
 
 	for (i = 0; i < height; i++)
-		free (grid[i]);
-	free (grid);
+		free(grid[i]);
+	free(grid);
 }
-	

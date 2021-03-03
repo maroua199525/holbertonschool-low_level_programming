29057b1cabb50911902c701ed78cca642aaa16f7
maroua199525 <2646@holbertonschool.com>
2021-03-03 12:51:51 +0000

    alloc-grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **array;
 
-	if (width <= 0 || height <=0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	array = malloc(sizeof(int) * height);

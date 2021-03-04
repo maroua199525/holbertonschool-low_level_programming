d9289d1add0025e1eed242c53d9d21feaa3e5cb7
maroua199525 <2646@holbertonschool.com>
2021-03-04 15:12:10 +0000

    nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+B#include "holberton.h"
 #include <stdlib.h>
 /**
  * string_nconcat - concat two strings
@@ -17,7 +17,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
 	if (s2 == NULL)
-		s2= "";
+		s2 = "";
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
 	if (n >= len2)

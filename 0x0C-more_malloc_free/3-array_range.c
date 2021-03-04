3dce86b27d9d39d902a1a6566f30062232535b9e
maroua199525 <2646@holbertonschool.com>
2021-03-04 19:09:40 +0000

    array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,9 +13,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	a = malloc((max - min) * sizeof(int));
-
-
+	a = malloc(((max - min) + 1) * sizeof(int));
 	if (a == NULL)
 		return (NULL);
 	for (i = min; i <= max; i++)
@@ -23,5 +21,5 @@ int *array_range(int min, int max)
 		a[k] = i;
 		k++;
 	}
-	return(a);
+	return (a);
 }

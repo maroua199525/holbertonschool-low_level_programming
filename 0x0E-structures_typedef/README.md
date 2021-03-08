0e058caf8fc16e718383d28c552034198c19bee2
maroua199525 <2646@holbertonschool.com>
2021-03-08 15:03:35 +0000

    header file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+Hello, we are going to discover What are structures, when, why and how to use them?
+How to use typede?
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif

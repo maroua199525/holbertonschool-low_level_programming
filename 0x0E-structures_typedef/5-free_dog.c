1f74bb880a457b5c188b0faaa89dd9034eaddf2d
maroua199525 <2646@holbertonschool.com>
2021-03-09 21:53:30 +0000

    free

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,16 +1,13 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- *free_dog - free dog 
+ *free_dog - free dog
  *@d:pointer
  *Return:void
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-	{
-	free (d->name);
-	free (d->owner);
-	free (d);
-	}
+	free(d->name);
+	free(d->owner);
+	free(d);
 }

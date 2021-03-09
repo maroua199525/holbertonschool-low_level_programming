b914d6326f53c46771bbf0c972b4ab8bc87511a7
maroua199525 <2646@holbertonschool.com>
2021-03-09 19:32:51 +0000

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -53,18 +53,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		dog->name = malloc(sizeof(char) * _strlen(name) + 1);
 		if (name == NULL)
-		return (NULL);
+		{
+			free(dog);
+			return (NULL);
+		}
 		else
 		_strcpy(dog->name, name);
 	}
 	if (owner == NULL)
-		dog->owner = NULL;
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
 	else
 	{
 		dog->owner = malloc(sizeof(char) * _strlen(owner) + 1);
 		if (dog->owner == NULL)
 			return (NULL);
-		else
 		_strcpy(dog->owner, owner);
 	}
 	dog->age = age;

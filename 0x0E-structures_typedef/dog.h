1e31a5e5298ed26f0cf6503d4cacb870fc1125d9
maroua199525 <2646@holbertonschool.com>
2021-03-09 20:00:17 +0000

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,7 +14,6 @@ typedef struct dog
 } dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-struct dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(struct dog_t *d);
-int _putchar(char c);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog( dog_t *d);
 #endif

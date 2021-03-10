1838b90278139574c81ed1baaa016aaf3a4b8c8f
maroua199525 <2646@holbertonschool.com>
2021-03-10 16:42:08 +0000

    int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,15 +5,15 @@
  *@array:int
  *@size:int
  *@cmp:int
- *Return:0
+ *Return:int_index
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, int_index;
 
-	if (cmp == NULL)
-		return (0);
 	if (size <= 0)
+		return (-1);
+	if (cmp == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{

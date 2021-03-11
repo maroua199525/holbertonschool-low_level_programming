3e74d361791ac6cd3ddc232b01aa3f654392a3a0
maroua199525 <2646@holbertonschool.com>
2021-03-11 18:21:44 +0000

    print-string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		str = va_arg(arg1, char *);
 		if (str == NULL)
 			printf("(nil)");
-		printf("%s", str);
+		else
+			printf("%s", str);
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}

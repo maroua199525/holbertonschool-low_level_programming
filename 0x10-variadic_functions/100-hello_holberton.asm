4a8be296c7657b5c3848d1c180d1001c5f07b21f
maroua199525 <2646@holbertonschool.com>
2021-03-11 21:16:27 +0000

    hello

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,11 +1,11 @@
 section .data
-msg db "Hello, Holberton ",10
+		msg db "Hello, Holberton ",10
 section .text
 global main
 main:
-		mov rax, 1;Sys_Write Function
-		mov rdi, 1;Std_Out File Descriptor
-		mov rsi, msg;Offset of msg
-		mov rdx, 17;Length Of msg
-		syscall	;Call the Kernel
+		mov rax, 1
+		mov rdi, 1
+		mov rsi, msg
+		mov rdx, 17
+		syscall
 

629b7007822e926151c3fe1104401b2ef7b45be6
maroua199525 <2646@holbertonschool.com>
2021-03-11 21:20:45 +0000

    hello

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,11 +1,12 @@
 section .data
-		msg db "Hello, Holberton ",10
-section .text
-global main
+		message db "Hello, Holberton",10
+
+		section .text
+		global main
+
 main:
-		mov rax, 1
 		mov rdi, 1
-		mov rsi, msg
+		mov rsi, message
+		mov rax, 1
 		mov rdx, 17
 		syscall
-

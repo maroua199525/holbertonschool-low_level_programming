38a3554ca2d3826f9c2a050a17a7844cdb1b7290
maroua199525 <2646@holbertonschool.com>
2021-03-18 15:46:51 +0000

    list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+B#include "lists.h"
+/**
+ *list_len - print a list
+ *@h:pointer of type list_t;
+ *Return: count
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+	if (h->str == NULL)
+		count++;
+	else
+		count++;
+	while (h->next != NULL)
+	{
+		if (h->next->str == NULL)
+			count++;
+		h = h->next;
+			count++;
+	}
+	return (count);
+}

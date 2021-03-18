de340fe0fb5ed76f781eeda877936231055acb0e
maroua199525 <2646@holbertonschool.com>
2021-03-18 22:58:48 +0000

    hh

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+void __attribute__((constructor)) f_print(void);
+
+/**
+ * f_print - prints  before the main function 
+ *
+ *Return: void
+ */
+
+void f_print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

175d217f2b9404c3eab71559a763acb7ec6672c4
maroua199525 <2646@holbertonschool.com>
2021-03-18 19:44:36 +0000

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,14 +6,14 @@
  */
 size_t print_list(const list_t *h)
 {
-	unsigned int count = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", (h)->len, (h)->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
 	}

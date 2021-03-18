9900968e8b4ad80b58c46a0c8204ebcfd4bc735e
maroua199525 <2646@holbertonschool.com>
2021-03-18 14:08:02 +0000

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ *print_list - print a list
+ *@h:pointer of type list_t;
+ *Return: count
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+	if ((*h).str == NULL)
+	{
+		printf("[0] (nil)\n");
+		count++;
+	}
+	else
+	{
+	printf("[%d] %s\n", (*h).len, (*h).str);
+	count++;
+	}
+	if (h->next != NULL)
+	{
+		printf("[%d] %s\n",h->next->len, h->next->str);
+		count++;
+	}
+	return (count);
+}

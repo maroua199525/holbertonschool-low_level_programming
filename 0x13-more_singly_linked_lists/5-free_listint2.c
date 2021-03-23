10d7d283abc630687c9e967020eed8920bbdbb04
maroua199525 <2646@holbertonschool.com>
2021-03-23 20:01:27 +0000

    free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@ void free_listint2(listint_t **head)
 {
 	 listint_t *ptr;
 
-	 if (head == NULL)
+	if (head == NULL)
 		return;
 	while (*head != NULL)
 	{
@@ -16,4 +16,5 @@ void free_listint2(listint_t **head)
 		*head = ptr->next;
 		free(ptr);
 	}
+	head = NULL;
 }

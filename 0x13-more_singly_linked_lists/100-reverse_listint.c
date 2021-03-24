39c2c8591cb099e9d2ea8f6ffec37518917e24a4
maroua199525 <2646@holbertonschool.com>
2021-03-24 11:27:42 +0000

    reverse

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ *reverse_listint - reverse the list;
+ *@head:pointer of type list_t;
+ *Return: count
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *ptr, *tmp;
+
+	if (*head == NULL || head == NULL)
+		return (NULL);
+	if ((*head)->next == NULL)
+		return (*head);
+	tmp = NULL;
+	while (*head != NULL)
+	{
+		ptr = (*head)->next;
+		(*head)->next = tmp;
+		tmp = *head;
+		*head = ptr;
+	}
+	*head = tmp;
+	return (*head);
+}

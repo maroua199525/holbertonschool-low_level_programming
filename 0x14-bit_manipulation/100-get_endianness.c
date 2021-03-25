86c6a9b9503b62eb447a672a9e5c1a161a24df35
maroua199525 <2646@holbertonschool.com>
2021-03-25 20:47:08 +0000

    get-endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * get_endianness - check the endianness
+ *
+ * Return: 1 or 0
+ */
+int get_endianness(void)
+{
+	int n = 1;
+
+	if(*(char *)&n == 1)
+		return (1);
+	else
+		return (0);
+}

2ac3d9864eb201bf4bc29a86123e5a975c279e58
maroua199525 <77970356+maroua199525@users.noreply.github.com>
2021-03-30 19:22:56 +0100

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ *create_file - create a new file
+ *@filename:string
+ *@text_content: string
+ *Return:1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+    int fd, wr, length;
+
+    for (length = 0; text_content[length] != '\0'; length++)
+        ;
+    if (filename == NULL)
+        return (-1);
+    fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+    if (fd == -1)
+        return (-1);
+    if (text_content != NULL)
+    {
+        wr = write(fd, text_content, 0);
+    }
+    if (wr == -1)
+        return (-1);
+    close(fd);
+    return (1);
+}

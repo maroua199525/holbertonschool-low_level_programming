91ac90e7ac0469e4623c35deecb65e8fe51763a9
maroua199525 <77970356+maroua199525@users.noreply.github.com>
2021-03-30 23:16:52 +0200

    betty fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
     
     if (filename == NULL)
         return (-1);
-    fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+    fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
     if (fd == -1)
         return (-1);
     if (text_content != NULL)

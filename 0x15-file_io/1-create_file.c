315a903b7aa8dc31f302467b1f132225972af7ba
maroua199525 <2646@holbertonschool.com>
2021-03-31 13:24:24 +0000

    fixed betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,21 +7,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, wr, length;
-    
-    if (filename == NULL)
-        return (-1);
-    fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
-    if (text_content != NULL)
-    {
-        for (length = 0; text_content[length] != '\0'; length++)
-        ;
-        wr = write(fd, text_content, length);
-    }
-    if (wr == -1)
-        return (-1);
-    close(fd);
-    return (1);
+	int fd, wr, length;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length] != '\0'; length++)
+			;
+		wr = write(fd, text_content, length);
+	}
+	if (wr == -1)
+		return (-1);
+	close(fd);
+	return (1);
 }

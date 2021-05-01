6baca15d136f9686868460e93417b20e3d26af88
maroua199525 <2646@holbertonschool.com>
2021-05-01 12:25:47 +0100

    add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,8 +15,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 	new_node->n = n;
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		new_node->next = NULL;
+		return (new_node);
+	}
+	new_node->prev = NULL;
 	new_node->next = *head;
 	*head = new_node;
-	new_node->prev = NULL;
 	return (new_node);
 }

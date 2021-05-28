6729641f78ee37a5b8ff0fbbe7161bf562aab468
maroua199525 <2646@holbertonschool.com>
2021-05-28 16:53:05 +0100

    printtable

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,30 +1,28 @@
 #include "hash_tables.h"
 /**
-*hash_table_set - adds an element to the hash table.
+*hash_table_print - function that prints a hash table.
 *@ht: hash_table_t
 * Return: Void
 */
 void hash_table_print(const hash_table_t *ht)
 {
-   
-    unsigned long int i = 0, size;
-   
-    if (ht == NULL)
-    {
-        return;
-    }
-    size = ht->size;
-    printf("{");
+	unsigned long int i = 0, size;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+	size = ht->size;
+	printf("{");
 	while (i <= size)
 	{
-        while (ht->array[i] != NULL)
-        {
-            if (i > 0)
-                printf(", ");
-		    printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
-            ht->array[i] = ht->array[i]->next;
-        }
-        i++;
+		while (ht->array[i] != NULL)
+		{
+			printf(", ");
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			ht->array[i] = ht->array[i]->next;
+		}
+		i++;
 	}
-    printf("}\n");
+	printf("}\n");
 }

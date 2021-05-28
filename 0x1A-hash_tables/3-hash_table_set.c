08773dc02e3372da4f7604955bdca635b50128e0
maroua199525 <2646@holbertonschool.com>
2021-05-28 12:49:23 +0100

    add size

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		return (0);
 	}
-	index = key_index((const unsigned char *)key, ht->);
+	index = key_index((const unsigned char *)key, ht->size);
 	ptr = ht->array[index];
 	if (ptr == NULL)
 	{

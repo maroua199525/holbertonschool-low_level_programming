6af56d096ec70e563085993971457ed03491204c
maroua199525 <2646@holbertonschool.com>
2021-05-28 12:51:47 +0100

    add function create

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -38,3 +38,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	tmp->next = create_node(key, value);
 	return (1);
 }
+/**
+* create_node -  create a node of type hash_node_t
+*@key: const char
+*@value: const char
+*Return: a pointer to the node
+*/
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	node->key = strdup(key);
+	if (node->key == NULL)
+		return (0);
+	node->value = strdup(value);
+	if (node->value == NULL)
+		return (0);
+	node->next = NULL;
+	return (node);
+}

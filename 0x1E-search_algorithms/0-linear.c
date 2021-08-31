954a21f77310947018b2c21fde3b9d127f03c4e5
maroua199525 <2646@holbertonschool.com>
2021-08-31 18:16:15 +0100

    add for bouvle

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,29 +5,21 @@
 * @array: an array of value
 * @size:  the number of elements in array
 * @value: the value to search for
-* Return: the index of the value
+* Return: the index of the value or -1
 */
 int linear_search(int *array, size_t size, int value)
 {
-
-	size_t i, index;
+	size_t i;
 
 	if (array == NULL)
 		return (-1);
-
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%ld] = [%d] \n", i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-		{
-			index = i;
+			return (i);
 
-		}
-		else
-		{
-			index = -1;
-		}
-		i++;
 	}
-	return (index);
+	return (-1);
+
 }

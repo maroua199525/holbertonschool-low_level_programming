fa4f737feb907386b1233ad1314f183724d62dbe
maroua199525 <2646@holbertonschool.com>
2021-08-31 14:27:40 +0100

    add size_t

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,13 +10,15 @@
 int linear_search(int *array, size_t size, int value)
 {
 
-	size_t i = 0;
-	int index = 0;
+	size_t i = 0, index = 0;
+
+	if (array == NULL)
+		return (-1);
 
 	while (i < size)
 	{
 		printf("Value checked array[%ld] = [%d] \n", i, array[i]);
-		if (value == array[i])
+		if (array[i] == value)
 		{
 			index = i;
 			break;
